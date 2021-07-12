6786e8efc263874c63e5406c65ce52e0c12a80d8
thesergioliveira <thesergioliveira@gmail.com>
2021-07-12 17:54:58 +0200

    fixing connection with api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment} from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import Loading from "./components/Loading";
 import Country from "./components/Country";
@@ -8,42 +8,45 @@ function App() {
   const [results, setResults] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    setTimeout(()=>{
+  useEffect(() => {
+    setTimeout(() => {
       setLoading(false);
-    }, 1000)
+    }, 1000);
   }, []);
 
-  function handleChange(e){
+  const getCountry = (status, countryName) => {
+    let textToUrl = encodeURIComponent(countryName);
+    let endPoint = `http://restcountries.eu/rest/v2/${status}/${textToUrl}`;
+
+    axios(endPoint)
+      .then(({ data }) => setResults(data))
+      .catch((err) => console.log(`There was an error ${err}`));
+  };
+
+  function handleChange(e) {
     setUserInput(e.target.value);
     console.log(userInput);
-
   }
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
-    let textToUrl = encodeURIComponent(userInput);
-    console.log(textToUrl);
-    let endPoint = `https://restcountries.eu/rest/v2/name/${textToUrl}`;
-    console.log(endPoint);
-    axios(endPoint)
-    .then(({data})=> setResults(data))
-    .catch((err)=> console.log(`There was an error: ${err}`));
-    console.log(results);
+    getCountry("name", userInput);
+    setUserInput("");
   }
- 
-  if (loading) return <Loading/>
+
+  if (loading) return <Loading />;
   return (
     <Fragment>
       <form onSubmit={handleSubmit}>
-        <input type="text"
+        <input
+          type="text"
           // value={userInput}
           onChange={handleChange}
           placeholder="Write a country name"
         ></input>
         <button type="submit">Search</button>
       </form>
-      <Country results={results}/>
+      <Country results={results} getCountry={getCountry} />
     </Fragment>
   );
 }
--- src/components/Country.js
+++ src/components/Country.js
@@ -1,25 +1,22 @@
 import React, { Fragment } from "react";
 
-const Country =(props)=>{
-    const lands = props.results.map((obj, i)=> {
-        const [name, area, capital, flag, population] = obj;
-        console.log(obj);
-        return (
-            <div key={i} className="country-container">
-                <div class="container-header">
-                    <h2 className="country-name">{name}</h2>
-                    <h4>Capital: {capital}</h4>
-                </div>
-                <img src={flag} alt={name}/>
-                <div className="container-footer">
-                    <span>Area: {area}</span> <span>Population: {population}</span>    
-                </div>                
-            </div>
-        );
-    });
+const Country = ({ results, getCountry }) => {
+  const lands = results.map((result, i) => {
+    let { name, area, capital, flag, population } = result;
     return (
-        <Fragment>{lands}</Fragment>
+      <div key={i} className="country-container">
+        <div class="container-header">
+          <h2 className="country-name">{name}</h2>
+          <h4>Capital: {capital}</h4>
+        </div>
+        <img src={flag} alt={name} />
+        <div className="container-footer">
+          <span>Area: {area}</span> <span>Population: {population}</span>
+        </div>
+      </div>
     );
-}
+  });
+  return <Fragment>{lands}</Fragment>;
+};
 
 export default Country;
